c9eab429e1c713d83c4b814326913ae103e44f2b
Mukela12 <Mukelathegreat@gmail.com>
2025-09-22 14:00:35 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import { createRequire } from 'module'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const require = createRequire(import.meta.url)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,5 +24,5 @@ export default defineConfig({
     alias: {
       '@cesdk-assets': join(__dirname, 'node_modules/@cesdk/cesdk-js/assets')
     }
-  }
-})
+  }                                                                                                                                                                             
+});
